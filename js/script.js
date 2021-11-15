0ea8c1c815afc0fbbdda4d71f0f251cb1d1e7872
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-11-15 18:42:52 +0100

    api success
    successfully adding image from api

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import { api } from "./settings/api.js";
 import { renderProducts } from "./ui/renderProducts.js";
 import { displayMessage } from "./components/displayMessage.js";
 
-export const productsUrl = api + "products/";
+export const productsUrl = api + "/" + "products/";
 export const cardContainer = document.querySelector(".card-container");
 const contentContainer = document.querySelector(".content");
 
--- js/ui/renderProducts.js
+++ js/ui/renderProducts.js
@@ -1,11 +1,15 @@
 import { cardContainer } from "../script.js";
+import { api } from "../settings/api.js";
 
 export function renderProducts(productsToRender) {
   console.log(`productsToRender`, productsToRender);
   cardContainer.innerHTML = "";
 
-  productsToRender.forEach(function (products) {
+  productsToRender.forEach(function (product) {
     cardContainer.innerHTML += `<div class="product-card">
+                                <div class="image-container">
+                               <img class="image" src="${api}${product.image[0].url}" alt=""${product.name}>
+                                </div>
                                 <h4>${product.name}</h4>
                     
                                     
