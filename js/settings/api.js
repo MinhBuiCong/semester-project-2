1c3882eff2d21a159cff0cfd1e69cbbb411191f0
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-12-13 10:19:53 +0100

    fixing style on navbar

--- js/settings/api.js
+++ js/settings/api.js
@@ -1,2 +1,2 @@
-// export const api = "http://localhost:1337";
-export const api = "https://semester-project-2-api.herokuapp.com";
+export const api = "http://localhost:1337";
+// export const api = "https://semester-project-2-api.herokuapp.com";
---
+++ js/shopping-cart.js
@@ -0,0 +1,7 @@
+import { navbar } from "./components/navbar.js";
+import { menu } from "./components/menu.js";
+import { footer } from "./components/footer.js";
+
+navbar();
+menu();
+footer();
