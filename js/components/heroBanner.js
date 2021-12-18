8d5438c0bf3b1bc348a8b4e2c5146aea47caba35
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2021-12-18 21:48:16 +0100

    Update heroBanner.js

--- js/components/heroBanner.js
+++ js/components/heroBanner.js
@@ -3,14 +3,15 @@ import { displayMessage } from "./common/displayMessage.js";
 
 const bannerContainer = document.querySelector(".main-content");
 
-const bannerUrl = api + "/" + "Banner/";
+const bannerUrl = api + "/" + "banner/";
+
 
 export async function getBanner() {
   try {
     const response = await fetch(bannerUrl);
     const banner = await response.json();
-    // const bannerImg = banner.image[0].url;
-    // bannerContainer.style.backgroundImage = `url("${api}${bannerImg}")`;
+    const bannerImg = banner.image[0].url;
+    bannerContainer.style.backgroundImage = `url("${api}${bannerImg}")`;
   } catch (error) {
     displayMessage(error, "404 - page not found", ".content");
   }
